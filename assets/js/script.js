9880ce1da6b46d65de22cdd5bd1b2ba7b2308506
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2023-12-11 13:59:23 -0500

    Updates to js 'skeleton'; declared most variables needed in script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,69 @@
 // Declare all variables
 var timerEl = document.getElementById("timer");
+var timeLeft = 75;
+var startContainer = document.getElementById("start-container");
 var startButton = document.getElementById("start-quiz");
 var questionContainer = document.getElementById("question-container");
 var question = document.getElementById("question");
+var answerChoices = document.getElementById("answer-choices");
+var answerResult = document.getElementById("answer-result");
+var scoreContainer = document.getElementById("score-container");
+var score = document.getElementById("current-score");
+var submitButton = document.getElementById("submit-button");
+var highScores = document.getElementById("highscore-list");
+var clearButton = document.getElementById("clear-button");
 
-var questions = [];
+// Store all questions in a single variable in array format
+var questions = [
+    {
+        question: "Question?",
+        choices: [ "A",
+                   "B",
+                   "C",
+                   "D"
+        ],
+        answer: 1
+    },
+    {
+        question: "Question?",
+        choices: [ "A",
+                   "B",
+                   "C",
+                   "D"
+        ],
+        answer: 1
+    },
+    {
+        question: "Question?",
+        choices: [ "A",
+                   "B",
+                   "C",
+                   "D"
+        ],
+        answer: 1
+    },
+    {
+        question: "Question?",
+        choices: [ "A",
+                   "B",
+                   "C",
+                   "D"
+        ],
+        answer: 1
+    },
+    {
+        question: "Question?",
+        choices: [ "A",
+                   "B",
+                   "C",
+                   "D"
+        ],
+        answer: 1
+    },
+];
 
 // Function for countdown timer
 function countdown() {
-    var timeLeft = 74;
     const timeInterval = setInterval(function() {
         if(timeLeft > 0) {
             timerEl.textContent = ("Time: " + timeLeft);
@@ -24,16 +79,31 @@ function countdown() {
 
 
 // Function to display questions
+function displayQuestion() {
+
+}
 
 
 // Function to get next question
-
+function getNextQuestion() {
+    
+}
 
 // Function to check answer
+function checkAnswer() {
+
+}
 
 
 // Function to save score
+function saveScore() {
+
+}
 
 
 // Event listeners for buttons
-startButton.addEventListener("click", countdown);
+startButton.addEventListener("click", function() {
+    countdown();
+    startContainer.style.display = "none";
+    questionContainer.classList.remove("hide");
+});
